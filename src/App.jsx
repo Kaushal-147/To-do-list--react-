ce30dd9c3f0f249e7dea592cea49a192b93b81bd
Kaushal <kaushal14707@gmail.com>
2025-01-04 22:38:45 +0530

    add all functionallity except local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,176 @@
-import { useState } from 'react'
 
+import { v4 as uuidv4 } from 'uuid';
 import Navbar from './components/Navbar'
+import { useState, useRef, useEffect } from 'react';
 
+function App() 
+{
+    const [todo , setTodo] = useState("");
+    const [todos , setTodos] = useState([]);
+    const [addsave , setAddsave] = useState("Add");
+    const [idfromtop , idsaver] = useState("");
+    const inputRef = useRef(null);
+    let date = new Date().toLocaleDateString();
 
-function App() {
-   let date = new Date().toLocaleDateString();
-   const [todo , setTodo] = useState(" ");
-   const [todos , setTodos] = useState([]);
+    const saveTodo = () => 
+    {
+        console.log(todos);
+        localStorage.setItem("todos", JSON.stringify(todos))
+    };
   
-   let handleAdd = () => {     
-                console.log(todo);
-                                 setTodos([...todos,{todo , isCompleted : false}]) 
-                                  setTodo(" ");                 };
-   let handleInputchange = (e) => { setTodo(e.target.value) };
-   let handleEdit = () => {};
-   let handleDelete = () => {};
-   let handleCheck = () => {};
-  return (
+    useEffect(() => 
+    {   
+        let todostring = localStorage.getItem("todos");
+        if (todostring) 
+        {
+            let todos = JSON.parse(localStorage.getItem("todos"));
+            setTodos(todos);
+        }
+    },     []);
+
+    let handleAdd = (e) => 
+    { 
+        if(todo === "" || todo === null || todo === " " ) return;
+        if(addsave === "Save") handleSave();
+        else
+        {
+            setTodos([...todos,{todo , isCompleted : false , id : uuidv4() , itemclass : "task-card bg-[#252E32] bg-opacity-[75%]  w-1/3 h-12 flex justify-between items-center p-4 rounded-3xl mb-3"}]); 
+            console.log(todos , "before save");
+            saveTodo();
+            console.log(todos , "after save");
+            console.log(localStorage.getItem("todos"));
+            setTodo(""); 
+        }
+    };
+
+    let handleInputchange = (e) => {setTodo(e.target.value)};
+
+    let handleEdit = (e) => 
+    { 
+        let id = e.target.name;
+        let index = todos.findIndex((item) => {return item.id === id});
+        idsaver(id) ;
+        if ( todos[index].isCompleted == true)
+        {
+            alert("You can't edit a completed task.");
+            return;
+        } 
+        else
+        { 
+            if (document.getElementById("Input").value === "" || document.getElementById("Input").value === " " || document.getElementById("Input").value === null || document.getElementById("Input").value === todos[index].todo) 
+            { 
+                todos[index].itemclass = "task-card bg-[#252E32] bg-opacity-[25%]  w-1/3 h-12 flex justify-between items-center p-4 rounded-3xl mb-3";
+                setTodo(todos[index].todo);
+                setAddsave("Save");
+                inputRef.current.focus(); 
+            }
+            else
+            {
+                if (confirm("Warning: Anything in Input will be overwritten."))
+                {
+                    todos[index].itemclass = "task-card bg-[#252E32] bg-opacity-[25%]  w-1/3 h-12 flex justify-between items-center p-4 rounded-3xl mb-3";
+                    setTodo(todos[index].todo);
+                    setAddsave("Save");
+                    inputRef.current.focus(); 
+                } 
+                else 
+                {
+                    return;
+                }
+            }
+        }
+    };
+
+    let handleSave = (e) => 
+    { 
+        let id = idfromtop;
+        let index = todos.findIndex((item) => {return item.id === id});
+        let newtodos = [...todos];
+        todos[index].itemclass = "task-card bg-[#252E32] bg-opacity-[75%]  w-1/3 h-12 flex justify-between items-center p-4 rounded-3xl mb-3";
+        newtodos[index].todo = document.getElementById("Input").value;
+        setTodos(newtodos);
+        saveTodo();
+        setTodo("");
+        setAddsave("Add"); 
+    }
+
+    let handleDelete = (e) => 
+    {     
+        if (confirm("Are you sure you want to delete this task?")) 
+        {
+            let id = e.target.name;
+            let newtodos = todos.filter((item)=>{return item.id !== id;});
+            console.log(newtodos);
+            setTodos([newtodos]);
+            console.log(todos, "handleDelete pachhi confirm kar etle newtodo delted");
+            saveTodo();
+        } 
+        else 
+        {
+            return;
+        }   
+    };
+
+    let handleCheck = (e) => 
+    {
+        let id = e.target.name;
+        let index = todos.findIndex((item) =>{ return item.id === id;});
+        let newtodos = [...todos];
+        newtodos[index].isCompleted = !newtodos[index].isCompleted;
+        setTodos(newtodos);
+    };
+
+    return (
     <div className="bg-gradient-to-r from-[#565657] to-[#092F41] min-h-screen flex flex-col  items-center ">
-      <Navbar />
-      <div className="container align-center min-w-full flex justify-center items-center">
-        <div className="Heading text-center justify-center text-[4.3rem] text-white mb-12 ">
-          Just do it.<span className='text-black'>|</span>  
+        <Navbar />
+        <div className="container align-center min-w-full flex justify-center items-center">
+          <div className="Heading text-center justify-center text-[4.3rem] text-white mb-12 ">
+            Just do it.
+            <span className='text-black'>|</span>  
+          </div>
         </div>
-      </div>
-      <div className="input-bar min-w-full flex justify-center items-center h-12 mb-6">
-        <input type="text" placeholder="Add a task"  onChange={handleInputchange} className="input-box rounded-xl h-3/4 w-1/3 rounded-tr-[0] rounded-br-[0] p-4" value={todo}  />
-        <button onClick={handleAdd}  className="add-button rounded-xl h-[76%] rounded-tl-[0] rounded-bl-[0] bg-black text-white w-16 hover:text-slate-300 active:text-white">
-          Add
-        </button>
-      </div>
-      <div className="date min-w-full flex justify-center items-center h-6 text-gray-400 text-[0.9rem] mb-3">
-        {date}
-      </div>
-      <div className="task-list min-w-full flex justify-center items-center flex-col">
-      {todos.map((item) => {
-        return (
-        <div className="task-card bg-slate-950  bg-opacity-60 w-1/3 h-12 flex justify-between items-center p-4 rounded-3xl mb-3">
-          <div className="check-box size-8 flex justify-center items-center">
-            <input type='checkbox' className='rounded bg-gray-800 size-[1.05rem]'></input>
-          </div>
-          <div className="task-name text-white">{item.todo}</div>
-          <div className="task-action flex flex-row justify-between items-center space-x-3">
-            <button className="edit-button  size-9 rounded-full   flex flex-row justify-center items-center">
-              <span className='size-9 hover:size-[2.252rem] active:size-9'>
-                <img src="  https://cdn-icons-png.flaticon.com/512/10336/10336582.png  " alt="edit" />
-              </span>
-            </button>
-            <button className="delete-button  size-9 rounded-full   flex flex-row justify-center items-center">
-              <span className='size-9 hover:size-[2.252rem] active:size-9'>
-                <img src=" https://cdn-icons-png.flaticon.com/512/10336/10336279.png " alt="delete" />
-              </span>
-            </button>
-          </div>
-        </div> )
-      }  ) }
-      
-      
-        
-      </div>
+        <div className="input-bar min-w-full flex justify-center items-center h-12 mb-6">
+           {/*--------------------------------------Main  Input bar -------------------------------------- */}
+          <input type="text" placeholder="Add a task" id="Input" onChange={handleInputchange} onKeyDown={(e) => {if(e.key === "Enter") handleAdd(); }} ref={inputRef} className="input-box rounded-xl h-3/4 w-1/3 rounded-tr-[0] rounded-br-[0] p-4" value={todo}  />
+           {/*-------------------------------------------------------------------------------------------- */}
+          <button onClick={handleAdd}  className="add-button rounded-xl h-[76%] rounded-tl-[0] rounded-bl-[0] bg-black text-white w-16 hover:text-slate-300 active:text-white">
+            {addsave}
+          </button>
+        </div>
+        <div className="date min-w-full flex justify-center items-center h-6 text-gray-400 text-[0.9rem] mb-3">
+          {date}
+        </div>
+        <div className="task-list min-w-full flex justify-center items-center flex-col">
+          {todos.map((item) => 
+          {
+            return (
+           <div key={item.id} className={item.itemclass} >
+               <div className="check-box size-8 flex justify-center items-center">
+                   <input type='checkbox' name={item.id} onChange={handleCheck} className="rounded bg-gray-800 size-[1.05rem]" value={item.isCompleted} >
+                   </input>
+               </div>
+               <div className={item.isCompleted?"line-through task-name text-[#999192]":"task-name text-white "}>
+                 {item.todo}
+               </div>
+               <div className="task-action flex flex-row justify-between items-center space-x-3">
+                   <button className="edit-button  size-9 rounded-full   flex flex-row justify-center items-center">
+                     <span className='size-9 hover:size-[2.252rem] active:size-9'>
+                     <img name={item.id}  onClick={handleEdit} src="../src/assets/edit.png" alt="edit" />
+                     </span>
+                   </button>
+                   <button  className="delete-button  size-9 rounded-full   flex flex-row justify-center items-center">
+                     <span className='size-9 hover:size-[2.252rem] active:size-9'>
+                     <img name={item.id} onClick={handleDelete} src="../src/assets/deelete.png " alt="delete" />
+                     </span>
+                   </button>
+               </div>
+            </div>
+                  )
+          }  ) }
+        </div>
     </div>
-  )
+           )
+           
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="min-w-full flex justify-start items-center">
-      <nav >
-          <div className="logo p-4">
-             <span><img src="      https://cdn-icons-png.flaticon.com/512/2512/2512687.png " className="bg-blend-lighten h-[3.2rem] w-[3.5rem] mx-8 cursor-pointer" /></span>
-          </div>
-      </nav>
-    </div>
+      <div className="min-w-full flex justify-start items-center">
+          <nav>
+              <div className="logo p-4">
+                  <span>
+                      <img src="https://cdn-icons-png.flaticon.com/512/2512/2512687.png" className="bg-blend-lighten h-[3.2rem] w-[3.5rem] mx-8 cursor-pointer" />
+                  </span>
+              </div>
+          </nav>
+      </div>
   )
 }
 
